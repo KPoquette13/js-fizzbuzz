891dae66c9e7be5823eed05fce797522402ef8b6
Kurt Poquette <kxp1607@rit.edu>
2015-08-01 12:44:32 -0500

    Updated fizzbuzz with input

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
 	$('#start-button').click(function() {
-		fizzBuzz();
+		var numValue = $('#max-num').val();
+		var decCheck = numValue.indexOf(".");
+		var maxNum = parseInt(numValue);
+
+		if(!(maxNum) || decCheck != -1){
+			alert('Please enter a valid whole number');
+		} else {
+			fizzBuzz(maxNum);
+		};
 	});
 
-	function fizzBuzz() {
-		for(var i = 1; i <= 100; i++){
+	function fizzBuzz(maxNum) {
+		for(var i = 1; i <= maxNum; i++){
 			if(i % 5 === 0 && i % 3 === 0){
 				$('.fb-list').append("FizzBuzz<br>");
 			} else if (i % 3 === 0){
